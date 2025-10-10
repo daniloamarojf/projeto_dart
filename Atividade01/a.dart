f2f5aed9453b58ec0dbb3408dbf276e6b3ccf5cc
daniloamarojf <daniloramarojf@gmail.com>
2025-10-09 23:32:40 -0300

    Comcluir exercicio b

---
+++ Atividade01/a.dart
@@ -0,0 +1,11 @@
+// Faça um programa que mostre a mensagem "Olá Mundo!" na tela.
+void main() {
+  String mensagem = "Olá Mundo!";
+
+  print('-' * 50);
+  print('Atividade "A" - Olá Mundo');
+  print('-' * 50);
+  print(mensagem);
+
+}
+
---
+++ Atividade01/b.dart
@@ -0,0 +1,48 @@
+//Faça um programa que peça 2 valores. Calcule e imprima  a soma, o produto,
+// a subtração, a divisão, o resto da divisão e a divisão inteira.
+import 'dart:io';
+
+void main() { 
+
+  print('-' * 50);
+  print('Atividade "B" - Cálculos Matemáticos');
+  print('-' * 50);
+
+  stdout.write('Digite o 1º valor: ');
+  int valor1 = int.parse(stdin.readLineSync()!);
+
+  stdout.write('Digite o 2º valor: ');
+  int valor2 = int.parse(stdin.readLineSync()!);
+
+  int soma = valor1 + valor2;
+  int subtracao = valor1 - valor2;
+  int produto = valor1 * valor2;
+  double divisao = valor1 / valor2;
+  int divisaoInteiro = valor1 ~/ valor2;
+  int divisaoResto = valor1 % valor2;
+
+  if (valor2 != 0) {
+    divisao = valor1 / valor2;
+    divisaoInteiro = valor1 ~/ valor2;
+    divisaoResto = valor1 % valor2;
+  } else {
+    divisao = 0;
+  }
+  
+
+  print('-' * 50);
+  print('Calculos');
+  print('-' * 50);
+  print('A soma é : $soma');
+  print('A subtração é: $subtracao');
+  print('A multiplicação é: $produto');
+  print('A divisão é: $divisao');
+  print('A divisão inteira é: $divisaoInteiro');
+  print('O resto da divisão é: $divisaoResto');
+  print('-' * 50);
+
+ 
+  
+  
+
+}
