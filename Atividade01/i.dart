3dd8edefb42d6526f268c3649dc3ca41babc4189
daniloamarojf <daniloramarojf@gmail.com>
2025-10-15 13:03:07 -0300

    Exercicio h concluido

--- Atividade01/i.dart
+++ Atividade01/i.dart
@@ -1,4 +1,39 @@
 // Faça um programa que receba um número inteiro. 
 //O programa deverá imprimir tabuada de multiplicação 
 //deste número.
+import 'dart:io';
+
+void main() {
+    print('-' * 50);
+    print('Exercício "I" - Tabuada de multiplicação');
+    print('-' * 50);
+
+    stdout.write('Deseja visualizar a tabuada de qual número?: ');
+    String? entrada = stdin.readLineSync();
+
+    if (entrada == null || entrada.isEmpty) {
+        print('Número não pode ser nulo.');
+        return;
+    }
+
+    int numero;
+    int resultado;
+
+    try {
+        numero = int.parse(entrada);
+    } catch (e) {
+        print('Digite um valor válido.');
+        return;
+    }
+
+    print('-' * 50);
+    print('TABUADA DE NÚMERO "${numero}"');
+    print('-' * 50);
+
+    for (int i = 1; i <= 10; i++) {
+        resultado = i * numero;
+        print('${i} x ${numero} = ${resultado}');
+    }
+}
+
 
---
+++ Atividade01/j.dart
@@ -0,0 +1,71 @@
+// Faça um programa que receba um valor em reais, 
+//depois calcule quantos euros, dólares e wons 
+//dariam para comprar com esse valor.
+import 'dart:io';
+
+void main() {
+  print('-' * 50);
+  print('Atividade "K" - Conversão de Moedas');
+  print('-' * 50);
+
+  stdout.write('Digite o valor em reais: ');
+  String? entrada = stdin.readLineSync();
+
+  if (entrada == null || entrada.isEmpty) {
+    print('Valor não pode ser nulo.');
+    return;
+  }
+
+  double valorReais;
+
+  try {
+    valorReais = double.parse(entrada);
+  } catch (e) {
+    print('Digite um valor válido');
+    return;
+  }
+
+  print('-' * 50);
+  print('1 - Dolar');
+  print('2 - Euro');
+  print('3 - Wons');
+
+  stdout.write('Escolha uma opção para converção: ');
+  String? numero = stdin.readLineSync();
+
+   if (numero == null || numero.isEmpty) {
+    print('Valor não pode ser nulo.');
+    return;
+  }
+
+  double opcao;
+
+  try {
+    opcao = double.parse(numero);
+  } catch (e) {
+    print('Digite um valor válido');
+    return;
+  }
+
+  
+
+
+  switch (opcao) {
+    
+    case 1:
+      double dolar = 5.47;
+      double ValorDolar = valorReais / dolar;
+      print('R\$ ${valorReais} equivale à \$ ${ValorDolar}');
+      break;
+    case 2:
+    double euro = 6.38;
+    double valorEuro = valorReais / euro;
+    print('R\$ ${valorReais} equilave à € ${valorEuro}');
+    break;
+    case 3:
+      double won = 0.004;
+      double valorWon = valorReais / won;
+      print('R\$ ${valorReais} equilave à w ${valorWon}');
+  }
+
+}
