cd899d458e8bafb7bb2b334f3bef128085bd1019
daniloamarojf <daniloramarojf@gmail.com>
2025-10-24 21:30:30 -0300

    Exercicio D - falta concluir

--- Atividade02/d.dart
+++ Atividade02/d.dart
@@ -4,9 +4,23 @@
 import 'dart:io';
 
 void main() {
+  print('-' * 50);
+  print('Verificação de Ano Bissexto.');
+  print('-' * 50);
 
+  while (true) {
+    int ano;
 
+    stdout.write('Digite o ano: ');
+    String? entrada = stdin.readLineSync();
+    
 
-  
+    stdout.write('Digite "s" para sair ou pressione Enter para continuar: ');
+    String? resposta = stdin.readLineSync();
 
+    if (resposta != null && resposta.toLowerCase() == 's') {
+      print('Saindo do programa ...');
+      break;
+    }
+  }
 }
