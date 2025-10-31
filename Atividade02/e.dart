eadc98254e6adde4ac1e90235afdd1a78095c9a7
daniloamarojf <daniloramarojf@gmail.com>
2025-10-31 20:44:17 -0300

    Falta concluir exercicio E

--- Atividade02/e.dart
+++ Atividade02/e.dart
@@ -10,7 +10,34 @@ void main() {
   print('-' * 50);
 
   while (true) {
-    stdout.write('Digite "s" para sair ou pressione Enter para continuar.');
+    stdout.write('Informe o tempo em segundos: ');
+    String? entrada1 = stdin.readLineSync();
+
+    stdout.write('Informe a aceleração: ');
+    String? entrada2 = stdin.readLineSync();
+
+    if (entrada1 == null ||
+        entrada1.isEmpty ||
+        entrada2 == null ||
+        entrada2.isEmpty) {
+      print('Valores não podem ser nulo.');
+      continue;
+    }
+    double tempo;
+    double aceleracao;
+
+    try {
+      tempo = double.parse(entrada1);
+      aceleracao = double.parse(entrada2);
+    } catch (e) {
+      print('Entrada inválida.');
+    }
+
+    for (int i = 0; i < tempo; i++) {
+      double velocidade = 
+      print('A velicidade é: ')
+    }
+      stdout.write('Digite "s" para sair ou pressione Enter para continuar.');
     String? resposta = stdin.readLineSync();
 
     if (resposta != null && resposta.toLowerCase() == 's') {
